1a1b55f0292307a8778138c269f8819c586b46bb
Tyler Ptak <tylerptak@gmail.com>
2025-09-27 20:34:17 -0500

    Bootstrap SimpleForm

--- src/Components/SimpleForm.js
+++
@@ -1,9 +0,0 @@
-export default function SimpleForm() {
-  return (
-    <>
-      <div id="formContainer">
-        <iframe id="form" title="myform" scrolling="no" src="https://docs.google.com/forms/d/e/1FAIpQLSdN7MPcIeZp6S3nvJqwGlFFiOHDQR0hOIx2wC47hEqOqBtqng/viewform?usp=dialog">Loading…</iframe>
-      </div>
-    </>
-  );
-}
